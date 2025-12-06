db15462bea1c38a63028ae8267c999b37cdf94c0
zoff99 <zoff99@users.noreply.github.com>
2025-12-06 14:44:36 +0100

    linux mad64 build ok

--- custom_docker/linux64/do.sh
+++ custom_docker/linux64/do.sh
@@ -7,13 +7,19 @@ export _HOME_
 echo "$_HOME_"
 cd "$_HOME_" || exit
 
-build_dir="./build/"
+build_dir=".build/"
 container_name="openssl_libs_lin_x86_64"
 
 if [ "$1""x" == "build-container""x" ]; then
     cp ../../000_deps/openssl.tar.gz . && docker build --progress=plain -f Dockerfile -t "$container_name" .
 else
+    os_="$2"
+    arch_="$3"
     mkdir -p "$build_dir"
     cp -v docker_build.sh "$build_dir"
-    docker run -v "$(pwd)""/""$build_dir":/work -i "$container_name" bash -c 'id -a; pwd; cd /source/openssl/ && /work/docker_build.sh'
+    docker run \
+        --env OS="$os_" \
+        --env ARCH="$arch_" \
+        -v "$(pwd)""/""$build_dir":/work -i "$container_name" \
+        bash -c 'id -a; pwd; cd /source/openssl/ && /work/docker_build.sh "$OS" "$ARCH"'
 fi
--- custom_docker/linux64/docker_build.sh
+++ custom_docker/linux64/docker_build.sh
@@ -3,15 +3,28 @@
 id -a
 pwd
 
+OS="$1"
+ARCH="$2"
+
+echo "OS  : ""$OS"
+echo "ARCH: ""$ARCH"
+
 ./Configure no-apps no-docs no-dso no-dgram --prefix=/opt/openssl --openssldir=/usr/local/ssl || exit 1
 make -j $(nproc) || exit 1
 make install || exit 1
 
 echo "/opt/openssl/include/"
-ls -al /opt/openssl/include/
+ls -al /opt/openssl/include/ || exit 1
 
 echo "/opt/openssl/lib64/"
-ls -al /opt/openssl/lib64/
+ls -al /opt/openssl/lib64/ || exit 1
 
 echo "/opt/openssl/lib64/pkgconfig/"
 ls -al /opt/openssl/lib64/pkgconfig/
+
+mkdir -p /work/ || exit 1
+cd /work/ && cp -av /opt/openssl/lib64/ . || exit 1
+mv -v /work/lib64 /work/lib || exit 1
+
+cd /work/ && cp -av /opt/openssl/include/ . || exit 1
+
