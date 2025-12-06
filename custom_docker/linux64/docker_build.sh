bd10867123757dba958be5e42c96fa7939fe1271
zoff99 <zoff99@users.noreply.github.com>
2025-12-06 14:51:03 +0100

    mostly finished linux amd64 build

--- custom_docker/linux64/docker_build.sh
+++ custom_docker/linux64/docker_build.sh
@@ -9,6 +9,9 @@ ARCH="$2"
 echo "OS  : ""$OS"
 echo "ARCH: ""$ARCH"
 
+# clean work directory from any previous runs
+rm -Rf /work/*
+
 ./Configure no-apps no-docs no-dso no-dgram --prefix=/opt/openssl --openssldir=/usr/local/ssl || exit 1
 make -j $(nproc) || exit 1
 make install || exit 1
@@ -22,9 +25,11 @@ ls -al /opt/openssl/lib64/ || exit 1
 echo "/opt/openssl/lib64/pkgconfig/"
 ls -al /opt/openssl/lib64/pkgconfig/
 
-mkdir -p /work/ || exit 1
+mkdir -p /work/
 cd /work/ && cp -av /opt/openssl/lib64/ . || exit 1
 mv -v /work/lib64 /work/lib || exit 1
 
 cd /work/ && cp -av /opt/openssl/include/ . || exit 1
 
+ls -al /work/lib/*.a || exit 1
+ls -hal /work/lib/*.a || exit 1
